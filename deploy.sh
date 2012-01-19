ba41d1154646361c6d9bffbba555205f363cb426
hatone <b1006006@gmail.com>
2012-01-18 19:20:44 -0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ export PATH=$PATH:/usr/local/rvm/rubies/ruby-1.9.2-p180/bin:$HOME/.gem/ruby/1.9.
 
 coffee -c *.coffee
 
-ls /usr/local/rvm/rubies/ruby-1.9.2-p180/bin
+ls -la /usr/local/rvm/rubies/ruby-1.9.2-p180/bin
