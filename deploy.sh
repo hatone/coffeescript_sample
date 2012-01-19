23947a3c1d929bc2f54df5c376b123ed83bb1b72
hatone <b1006006@gmail.com>
2012-01-18 18:11:40 -0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,5 @@ export GEM_HOME=$HOME/.gem/ruby/1.9.1
 export GEM_PATH=$GEM_HOME
 export PATH=$PATH:/usr/local/rvm/rubies/ruby-1.9.2-p180/bin:$HOME/.gem/ruby/1.9.1/bin
 
-gem install bundler --no-ri --no-rdoc
+coffee -c *.coffee
 
-bundle install --without development
