142373d720e416d4df47d191e2366e2ea39c2ca6
hatone <b1006006@gmail.com>
2012-01-18 17:48:18 -0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ export GEM_HOME=$HOME/.gem/ruby/1.9.1/
 export GEM_PATH=$GEM_HOME
 export PATH=$PATH:/usr/local/rvm/rubies/ruby-1.9.2-p180/bin:$HOME/.gem/ruby/1.9.1/bin
 
-coffee -c *.coffee
+$HOME/.gem/ruby/1.9.1/bin/coffee -c *.coffee
