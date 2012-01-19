1b5498420a96f6fb47c0805af6bc174ef105f71e
hatone <b1006006@gmail.com>
2012-01-18 19:47:41 -0800

    p

--- setup.sh
+++ setup.sh
@@ -2,9 +2,8 @@ export GEM_HOME=$HOME/.gem/ruby/1.9.1/
 export GEM_PATH=$GEM_HOME
 export PATH=$PATH:/usr/local/rvm/rubies/ruby-1.9.2-p180/bin:$HOME/.gem/ruby/1.9.1/bin
 
-rvm gemset create hatone
-rvm gemset use    hatone
-
 gem install coffee-script
+gem install guard-coffeescript
+gem install rb-fsevent
 
-rvm gemset list
+guard
