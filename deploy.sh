680e3bc864d8b670ad7b43bc7e50a85c7d918f03
hatone <b1006006@gmail.com>
2012-01-18 22:30:55 -0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,2 @@
-export GEM_HOME=$HOME/.gem/ruby/1.9.1
-export GEM_PATH=$GEM_HOME
-export PATH=$PATH:/usr/local/rvm/rubies/ruby-1.9.2-p180/bin:$HOME/.gem/ruby/1.9.1/bin
-
-find . |xargs touch
-
+export PATH=$PATH:$HOME/node_modules/.bin
+coffee -v
--- setup.sh
+++ setup.sh
@@ -2,8 +2,4 @@ export GEM_HOME=$HOME/.gem/ruby/1.9.1/
 export GEM_PATH=$GEM_HOME
 export PATH=$PATH:/usr/local/rvm/rubies/ruby-1.9.2-p180/bin:$HOME/.gem/ruby/1.9.1/bin
 
-gem install coffee-script --no-ri --no-rdoc
-gem install guard-coffeescript --no-ri --no-rdoc
-gem install rb-fsevent --no-ri --no-rdoc
-
-guard
+npm install coffee-script
